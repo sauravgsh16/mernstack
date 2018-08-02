4aa9e7d16eac2bc1fb87e171ba4dc8e90109f605
Saurav Ghosh <sauravgsh16@gmail.com>
2018-08-03 04:04:55 +0530

    Register and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from '../src/components/layout/Navbar';
 import Footer from '../src/components/layout/Footer';
 import Landing from '../src/components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div>
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/register' component={Register} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
--- validation/register.js
+++ validation/register.js
@@ -10,7 +10,7 @@ module.exports = function validateRegisterInput(data) {
   data.password2 = !isEmpty(data.password2) ? data.password2 : '';
 
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
-    errors.name = 'Name should be of between 2 and 30 characters';
+    errors.name = 'Name should be between 2 and 30 characters';
   }
 
   if (Validator.isEmpty(data.name)) {
