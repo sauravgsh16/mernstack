fbbcabb47016ff081a5c3ebef20c5aa19503d3b9
Saurav Ghosh <sauravgsh16@gmail.com>
2018-08-14 03:26:29 +0530

    Functionality to add exp

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -60,6 +60,19 @@ export const deleteProfile = () => dispatch => {
   }
 };
 
+// Add Experience
+export const addExperience = (expData, history) => dispatch => {
+  axios
+    .post('/api/profile/experience', expData)
+    .then(res => history.push('/dashboard'))
+    .catch(err => 
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+}
+
 // Profile loading
 export const setProfileLoading = () => {
   return {
