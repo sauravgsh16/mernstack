8081df428f2ad16c86a471a9abb9efa6aedf21e8
Saurav Ghosh <sauravgsh16@gmail.com>
2018-08-20 00:12:45 +0530

    crendtails update on profile page

--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -6,52 +6,69 @@ class ProfileCreds extends Component {
   render() {
 
     const { experience, education } = this.props;
+    const expItem = experience.map(exp => {
+      return (
+        <li key={exp._id} className="list-group-item">
+          <h4>{exp.company}</h4>
+          <p>
+            <Moment format="DD/MM/YYYY">{exp.from}</Moment> - 
+            { exp.to === null ? (<span>Current</span>) :
+              <Moment format="DD/MM/YYYY">{exp.to}</Moment>
+            }
+          </p>
+          <p>
+            <strong>Position: </strong> {exp.title} 
+          </p>
+          <p>
+            {exp.location === "" ? null :
+              <span><strong>Location: </strong>{exp.location}</span>
+            }
+          </p>
+          { exp.description === "" ? null :
+            <span><strong>Description:</strong> {exp.description}</span>
+          }
+        </li>
+      );
+    });
+    
+    const eduItem = education.map(edu => {
+      return (
+        <li key={edu._id} className="list-group-item">
+          <h4>{edu.school}</h4>
+          <p>
+            <Moment format="DD/MM/YYYY">{edu.from}</Moment> - 
+            { edu.to === null ? (<span>Current</span>) :
+              <Moment format="DD/MM/YYYY">{edu.to}</Moment>
+            }
+          </p>
+          <p>
+            <strong>Degree: </strong> {edu.degree} 
+          </p>
+          <p>
+            <strong>Field of Study: </strong> {edu.fieldofstudy} 
+          </p>
+          { edu.description === "" ? null :
+            <span><strong>Description:</strong> {edu.description}</span>
+          }
+        </li>
+      );
+    });
 
     return (
-      <div class="row">
-        <div class="col-md-6">
-          <h3 class="text-center text-info">Experience</h3>
-          <ul class="list-group">
-            <li class="list-group-item">
-              <h4>Microsoft</h4>
-              <p>Oct 2011 - Current</p>
-              <p>
-                <strong>Position:</strong> Senior Developer
-                  </p>
-              <p>
-                <strong>Description:</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde doloribus dicta enim
-                    excepturi laborum voluptatem nam provident quisquam facere. Quae?</p>
-            </li>
-            <li class="list-group-item">
-              <h4>Sun Microsystems</h4>
-              <p>Oct 2004 - Nov 2011</p>
-              <p>
-                <strong>Position: </strong> Systems Admin</p>
-              <p>
-                <p>
-                  <strong>Location: </strong> Miami, FL
-                    </p>
-                <strong>Description: </strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde doloribus dicta
-                    enim excepturi laborum voluptatem nam provident quisquam facere. Quae?</p>
-            </li>
-          </ul>
+      <div className="row">
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Experience</h3>
+          { expItem.length > 0 ? 
+            <ul className="list-group">{expItem}</ul> :
+            <p className="text-center">No experience listed</p>
+          }
         </div>
-        <div class="col-md-6">
-          <h3 class="text-center text-info">Education</h3>
-          <ul class="list-group">
-            <li class="list-group-item">
-              <h4>Univeresity Of Washington</h4>
-              <p>Sep 1993 - June 1999</p>
-              <p>
-                <strong>Degree: </strong>Masters</p>
-              <p>
-                <strong>Field Of Study: </strong>Computer Science</p>
-
-              <p>
-                <strong>Description:</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde doloribus dicta
-                      enim excepturi laborum voluptatem nam provident quisquam facere. Quae?</p>
-            </li>
-          </ul>
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Education</h3>
+          { eduItem.length > 0 ? 
+            <ul className="list-group">{eduItem}</ul> :
+            <p className="text-center">No education listed</p>
+          }
         </div>
       </div>
     );
